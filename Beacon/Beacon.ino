fc35a9e499b49afe78a89443574d8c0c9c69aaa2
Sergii Bomko <aquila.ua@gmail.com>
2012-10-29 04:30:45 +0200

    added max delay time

--- Beacon/Beacon.ino
+++ Beacon/Beacon.ino
@@ -1,18 +1,21 @@
 const int gerkonPin = 10;
-
-int length = 9;
 int pins[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+int length = 9;
+int defaultCurrentPos = 6;
 int currentPos = 5;
-int delayTime = 1000;
+int defaultDelayTime = 1000;
+int delayTime = defaultDelayTime;
 int gerkonState = 0; 
 int prevGerkonState = 0;
+int minDelayTime = 10;
+int maxDelayTime = 10000;
+int sectorRate = 8;
 unsigned long lastStartTime = 0;
 unsigned long prevMillis = 0;
 
 void setup() {
-  for (int i = 0; i < length; i = i + 1) {
+  for (int i = 0; i < length; i = i + 1)
     pinMode(pins[i], OUTPUT);
-  }  
   pinMode(gerkonPin, INPUT);
 }
 
@@ -22,57 +25,47 @@ void loop() {
   
   if(gerkonState == HIGH && prevGerkonState == LOW)
   {
-    reset();
+    currentPos = defaultCurrentPos;
     if(lastStartTime != 0)
     {
-      long dTime = (currentMillis - lastStartTime) / 8;
-      if(dTime < 10)
+      long dTime = (currentMillis - lastStartTime) / sectorRate;
+      if(dTime < minDelayTime)
         goto out;
       delayTime = dTime;
     }
     lastStartTime = currentMillis;
   }
   prevGerkonState = gerkonState;
+  if((currentMillis - lastStartTime) > maxDelayTime)
+  {
+    clearAll();
+    delayTime = defaultDelayTime;
+    return;
+  }
   out:
   goWave(delayTime, currentMillis);
-}
-
-void reset()
-{
-  currentPos = 6;
 }
 
 void goWave(long delayTime, long currentMillis)
 {
   if(currentMillis - prevMillis > delayTime) {
-  
     prevMillis = currentMillis;
-    if(currentPos > length - 1)
-    {
-      currentPos = 0;
-    }
-    
     clearAll();
     digitalWrite(pins[currentPos], HIGH);
-    digitalWrite(pins[getPos(currentPos + 1) ], HIGH);
-    
-    currentPos = currentPos + 1;
+    currentPos = getPos(currentPos + 1);
+    digitalWrite(pins[currentPos], HIGH);    
   }
 }
 
 void clearAll()
 {
-  for (int i = 0; i < length; i = i + 1) {
+  for (int i = 0; i < length; i = i + 1)
     digitalWrite(pins[i], LOW);
-  } 
 }
 
 int getPos(int pos)
 {
   if(pos > length - 1)
-  {
-    return pos - length;
-  }
-  
+    pos =  pos - length;
   return pos;
 }
